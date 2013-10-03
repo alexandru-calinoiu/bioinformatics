45230ce7577a6ae540198e5b5f6f46ab197009fc
Calin <calinoiu.alexandru@agilefreaks.com>
2013-10-03 14:40:47 +0300

    adds solution to freq words problem

--- lib/freq_words/freq_words.rb
+++ lib/freq_words/freq_words.rb
@@ -1,2 +1,28 @@
 class FreqWords
+  attr_reader :k, :text
+
+  def initialize(k, text)
+    #noinspection RubyInstanceVariableNamingConvention
+    @k = k
+    @text = text
+  end
+
+  def output
+    result = Hash.new(0)
+    max = 0
+
+    @text.chars.each_index do |index|
+      break if index + @k > @text.length
+      key = @text[index, @k].to_sym
+
+      result[key] += 1
+
+      max = result[key] > max ? result[key] : max
+    end
+
+    result.delete_if { |key, value| value != max }.keys.map { |k| k.to_s }
+  end
 end
+
+#lines = File.readlines('stepic_dataset.txt')
+#p FreqWords.new(lines[1].to_i, lines[0]).output
--- spec/freq_words/freq_words_spec.rb
+++ spec/freq_words/freq_words_spec.rb
@@ -1,4 +1,33 @@
 require 'spec_helper'
 
 describe FreqWords do
+  subject { FreqWords.new(k, text).output }
+
+  context 'when there is there is only 1 result' do
+    let(:k) { 1 }
+    let(:text) { 'A' }
+
+    it { should == %w(A) }
+  end
+
+  context 'when there are 2 results and length 1' do
+    let(:k) { 1 }
+    let(:text) { 'AC' }
+
+    it { should == %w(A C) }
+  end
+
+  context 'when there k is greater then 1' do
+    let(:k) { 2 }
+    let(:text) { 'AC' }
+
+    it { should == %w(AC) }
+  end
+
+  context 'it should return the most frequent k-mers' do
+    let(:k) { 2 }
+    let(:text) { 'ACGTAC' }
+
+    it { should == %w(AC) }
+  end
 end
